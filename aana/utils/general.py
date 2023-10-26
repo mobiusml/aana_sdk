3d176f23de8dc32b21bc7f18afe87d405fc6861a
movchan74 <aleksandr@mobius.ml>
2023-10-26 19:00:24 +0000

    Applied PR suggestions

--- aana/utils/general.py
+++ aana/utils/general.py
@@ -1,53 +1,6 @@
-import pickle
-from typing import Any, TypeVar
+from typing import TypeVar
 
 from pydantic import BaseModel
-
-
-def load_options(s: str, ignore_errors: bool = True) -> Any:
-    """
-    Load options from a string.
-
-    The string is assumed to be a pickled object encoded in latin1.
-    If the string cannot be unpickled, return the string itself if ignore_errors is True,
-    otherwise raise an exception.
-
-    The function is used to pass options using Ray's user_config.
-    user_config accepts only JSON serializable objects, so we need to encode the options.
-
-    Args:
-        s (str): string to be unpickled
-        ignore_errors (bool): if True, return the string itself if it cannot be unpickled, otherwise raise an exception
-
-    Returns:z
-        unpickled object or the string itself if ignore_errors is True
-    """
-    try:
-        b = s.encode("latin1")
-        return pickle.loads(b)
-    except Exception as e:
-        if ignore_errors:
-            return s
-        raise e
-
-
-def encode_options(options: Any) -> str:
-    """
-    Encode options as a string.
-
-    The string is a pickled object encoded in latin1.
-
-    The function is used to pass options using Ray's user_config.
-    user_config accepts only JSON serializable objects, so we need to encode the options.
-
-    Args:
-        options (Any): options to be encoded
-
-    Returns:
-        str: options encoded as a string
-    """
-    b = pickle.dumps(options)
-    return b.decode("latin1")
 
 
 OptionType = TypeVar("OptionType", bound=BaseModel)
