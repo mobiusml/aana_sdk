6f99c495e1681c501aa7d212be2facaf7dc67c11
movchan74 <aleksandr@mobius.ml>
2023-10-26 17:02:04 +0000

    App test

--- aana/main.py
+++ aana/main.py
@@ -1,9 +1,12 @@
 from aana.api.request_handler import RequestHandler
+from aana.configs.endpoints import endpoints
 from aana.configs.deployments import deployments
+from aana.configs.pipeline import nodes as pipeline_nodes
 
 # TODO: add build system to only serve the deployment if it's needed
-binded_deployments = {}
-for name, deployment in deployments.items():
-    binded_deployments[name] = deployment.bind()
 
-server = RequestHandler.bind(binded_deployments)
+context = {
+    "deployments": {name: deployment.bind() for name, deployment in deployments.items()}
+}
+
+server = RequestHandler.bind(endpoints, pipeline_nodes, context)
