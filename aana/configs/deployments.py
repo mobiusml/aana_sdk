be2e957bf0694e35c9ec0d96872e90a43997ef5d
movchan74 <aleksandr@mobius.ml>
2023-11-01 10:14:36 +0000

    Basic build system

--- aana/configs/deployments.py
+++ aana/configs/deployments.py
@@ -18,4 +18,18 @@ deployments = {
             ),
         },
     ),
+    "vllm_deployment_zephyr_7b_beta": VLLMDeployment.options(
+        num_replicas=1,
+        max_concurrent_queries=1000,
+        ray_actor_options={"num_gpus": 0.5},
+        user_config={
+            "model": "TheBloke/zephyr-7B-beta-AWQ",
+            "dtype": "auto",
+            "quantization": "awq",
+            "gpu_memory_utilization": 0.7,
+            "default_sampling_params": encode_options(
+                SamplingParams(temperature=1.0, top_p=1.0, top_k=-1, max_tokens=256)
+            ),
+        },
+    ),
 }
