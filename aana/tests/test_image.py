72fed261de7f666dcda6b1559ce1dfe5074df42f
movchan74 <aleksandr@mobius.ml>
2023-11-03 13:59:44 +0000

    Tests and bug fixes for Image and ImageInput.

---
+++ aana/tests/test_image.py
@@ -0,0 +1,170 @@
+from importlib import resources
+from pathlib import Path
+import numpy as np
+import pytest
+from aana.models.core.image import Image
+
+
+def load_numpy_from_image_bytes(content: bytes) -> np.ndarray:
+    """
+    Load a numpy array from image bytes.
+    """
+    image = Image(content=content, save_on_disc=False)
+    return image.get_numpy()
+
+
+def test_image():
+    """
+    Test that the image can be created from path, url, content, or numpy.
+    """
+
+    try:
+        path = resources.path("aana.tests.files.images", "Starry_Night.jpeg")
+        image = Image(path=path, save_on_disc=False)
+        assert image.path == path
+        assert image.content is None
+        assert image.numpy is None
+        assert image.url is None
+
+        numpy = image.get_numpy()
+        assert numpy.shape == (720, 909, 3)
+
+        content = image.get_content()
+        numpy = load_numpy_from_image_bytes(content)
+        assert numpy.shape == (720, 909, 3)
+    finally:
+        image.cleanup()
+
+    try:
+        url = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/909px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"
+        image = Image(url=url, save_on_disc=False)
+        assert image.path is None
+        assert image.content is None
+        assert image.numpy is None
+        assert image.url == url
+
+        numpy = image.get_numpy()
+        assert numpy.shape == (720, 909, 3)
+
+        content = image.get_content()
+        numpy = load_numpy_from_image_bytes(content)
+        assert numpy.shape == (720, 909, 3)
+    finally:
+        image.cleanup()
+
+    try:
+        path = resources.path("aana.tests.files.images", "Starry_Night.jpeg")
+        content = path.read_bytes()
+        image = Image(content=content, save_on_disc=False)
+        assert image.path is None
+        assert image.content == content
+        assert image.numpy is None
+        assert image.url is None
+
+        numpy = image.get_numpy()
+        assert numpy.shape == (720, 909, 3)
+
+        content = image.get_content()
+        numpy = load_numpy_from_image_bytes(content)
+        assert numpy.shape == (720, 909, 3)
+    finally:
+        image.cleanup()
+
+    try:
+        numpy = np.random.rand(100, 100, 3).astype(np.uint8)
+        image = Image(numpy=numpy, save_on_disc=False)
+        assert image.path is None
+        assert image.content is None
+        assert np.array_equal(image.numpy, numpy)
+        assert image.url is None
+
+        numpy = image.get_numpy()
+        assert np.array_equal(numpy, numpy)
+
+        content = image.get_content()
+        numpy = load_numpy_from_image_bytes(content)
+        assert np.array_equal(numpy, numpy)
+    finally:
+        image.cleanup()
+
+
+def test_image_path_not_exist():
+    """
+    Test that the image can't be created from path if the path doesn't exist.
+    """
+    path = Path("path/to/image_that_does_not_exist.jpeg")
+    with pytest.raises(FileNotFoundError):
+        Image(path=path)
+
+
+def test_save_image():
+    """
+    Test that save_on_disc works.
+    """
+
+    try:
+        path = resources.path("aana.tests.files.images", "Starry_Night.jpeg")
+        image = Image(path=path, save_on_disc=True)
+        assert image.path == path
+        assert image.content is None
+        assert image.numpy is None
+        assert image.url is None
+        assert image.path.exists()
+    finally:
+        image.cleanup()
+
+    try:
+        url = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1024px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"
+        image = Image(url=url, save_on_disc=True)
+        assert image.content is None
+        assert image.numpy is None
+        assert image.url == url
+        assert image.path.exists()
+    finally:
+        image.cleanup()
+
+    try:
+        path = resources.path("aana.tests.files.images", "Starry_Night.jpeg")
+        content = path.read_bytes()
+        image = Image(content=content, save_on_disc=True)
+        assert image.content == content
+        assert image.numpy is None
+        assert image.url is None
+        assert image.path.exists()
+    finally:
+        image.cleanup()
+
+    try:
+        numpy = np.random.rand(100, 100, 3).astype(np.uint8)
+        image = Image(numpy=numpy, save_on_disc=True)
+        assert image.content is None
+        assert np.array_equal(image.numpy, numpy)
+        assert image.url is None
+        assert image.path.exists()
+    finally:
+        image.cleanup()
+
+
+def test_cleanup():
+    """
+    Test that cleanup works.
+    """
+
+    try:
+        url = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1024px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"
+        image = Image(url=url, save_on_disc=True)
+        assert image.path.exists()
+    finally:
+        image.cleanup()
+        assert not image.path.exists()
+
+
+def test_at_least_one_input():
+    """
+    Test that at least one input is provided.
+    """
+    with pytest.raises(ValueError):
+        Image(save_on_disc=False)
+
+    with pytest.raises(ValueError):
+        Image(save_on_disc=True)
