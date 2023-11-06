48e8d1eba7fbec66b9b365b03fed36ed7a0f2485
movchan74 <aleksandr@mobius.ml>
2023-11-06 16:30:47 +0000

    Added setting for the app

---
+++ aana/tests/test_settings.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from aana.configs.settings import Settings
+
+
+def test_default_tmp_data_dir():
+    """Test that the default temporary data directory is set correctly."""
+    settings = Settings()
+    assert settings.tmp_data_dir == Path("/tmp/aana/data")
+
+
+def test_custom_tmp_data_dir(monkeypatch):
+    """Test that the custom temporary data directory with environment variable is set correctly."""
+    test_path = "/tmp/override/path"
+    monkeypatch.setenv("TMP_DATA_DIR", test_path)
+    settings = Settings()
+    assert settings.tmp_data_dir == Path(test_path)
