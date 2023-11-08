774ad9c1d27a47a4fff2fac24cf8fac7bf56e9c5
movchan74 <aleksandr@mobius.ml>
2023-11-08 12:53:44 +0000

    Fixed pydantic serializartion issue. Whisper is running now.

--- aana/api/responses.py
+++ aana/api/responses.py
@@ -1,6 +1,33 @@
 from typing import Any, Optional
 from fastapi.responses import JSONResponse
 import orjson
+from pydantic import BaseModel
+
+
+def orjson_default(obj: Any) -> Any:
+    """
+    Default function for orjson.dumps to handle pydantic models.
+
+    If orjson does not know how to serialize an object, it calls the default function.
+
+    If we see that the object is a pydantic model,
+    we call the dict method to get the dictionary representation of the model that orjson can serialize.
+
+    If the object is not a pydantic model, we raise a TypeError.
+
+    Args:
+        obj (Any): The object to serialize.
+
+    Returns:
+        Any: The serialized object.
+
+    Raises:
+        TypeError: If the object is not a pydantic model.
+    """
+
+    if isinstance(obj, BaseModel):
+        return obj.dict()
+    raise TypeError
 
 
 class AanaJSONResponse(JSONResponse):
@@ -23,4 +50,4 @@ class AanaJSONResponse(JSONResponse):
         """
         Override the render method to use orjson.dumps instead of json.dumps.
         """
-        return orjson.dumps(content, option=self.option)
+        return orjson.dumps(content, option=self.option, default=orjson_default)
