8e93fded437ced09cf12260852c84a4cac632c94
Aleksandr Movchan <aleksandr@mobiuslabs.com>
2023-11-10 14:10:52 +0000

    Changed methods of BLIP2 deployment to be consistent with VLLM deployment

--- aana/tests/deployments/test_hf_blip2_deployment.py
+++ aana/tests/deployments/test_hf_blip2_deployment.py
@@ -21,7 +21,11 @@ def ray_setup(deployment):
 
 @pytest.mark.skipif(not is_gpu_available(), reason="GPU is not available")
 @pytest.mark.asyncio
-async def test_hf_blip2_deployments():
+@pytest.mark.parametrize(
+    "image_name, expected_text",
+    [("Starry_Night.jpeg", "the starry night by vincent van gogh")],
+)
+async def test_hf_blip2_deployments(image_name, expected_text):
     for name, deployment in deployments.items():
         # skip if not a VLLM deployment
         if deployment.name != "HFBlip2Deployment":
@@ -29,14 +33,18 @@ async def test_hf_blip2_deployments():
 
         handle = ray_setup(deployment)
 
-        path = resources.path("aana.tests.files.images", "Starry_Night.jpeg")
+        path = resources.path("aana.tests.files.images", image_name)
         image = Image(path=path, save_on_disk=False)
+
+        output = await handle.generate.remote(image=image)
+        caption = output["caption"]
+        compare_texts(expected_text, caption)
 
         images = [image] * 8
 
-        output = await handle.generate_captions.remote(images=images)
+        output = await handle.generate_batch.remote(images=images)
         captions = output["captions"]
 
         assert len(captions) == 8
         for caption in captions:
-            compare_texts("the starry night by vincent van gogh", caption)
+            compare_texts(expected_text, caption)
