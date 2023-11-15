3a6f0620c6b00915190ae89a45dd8ae444cd6778
movchan74 <aleksandr@mobius.ml>
2023-11-15 13:21:17 +0000

    Moved from youtube specific code to generic video downloader

--- aana/tests/test_video.py
+++ aana/tests/test_video.py
@@ -4,8 +4,8 @@ import pytest
 from aana.configs.settings import settings
 from aana.exceptions.general import DownloadException
 from aana.models.core.video import Video
-from aana.models.pydantic.video_input import VideoInput, YoutubeVideoInput
-from aana.utils.video import download_youtube_video
+from aana.models.pydantic.video_input import VideoInput
+from aana.utils.video import download_video
 
 
 @pytest.fixture
@@ -145,30 +145,41 @@ def test_at_least_one_input():
         Video(save_on_disk=True)
 
 
-def test_download_youtube_video():
+def test_download_video(mock_download_file):
     """
-    Test that download_youtube_video works for both VideoInput and YoutubeVideoInput.
+    Test download_video.
     """
     # Test VideoInput
     path = resources.path("aana.tests.files.videos", "squirrel.mp4")
     video_input = VideoInput(path=str(path))
-    video = download_youtube_video(video_input)
+    video = download_video(video_input)
     assert isinstance(video, Video)
     assert video.path == path
     assert video.content is None
     assert video.url is None
 
-    # Test YoutubeVideoInput
+    try:
+        url = "http://example.com/squirrel.mp4"
+        video_input = VideoInput(url=url)
+        video = download_video(video_input)
+        assert isinstance(video, Video)
+        assert video.path is not None
+        assert video.content is None
+        assert video.url == url
+        assert video.path.exists()
+    finally:
+        video.cleanup()
+
+    # Test Youtube URL
     youtube_url = "https://www.youtube.com/watch?v=yModCU1OVHY"
-    tmp_dir = settings.tmp_data_dir
-    youtube_video_dir = tmp_dir / "youtube_videos"
+    youtube_video_dir = settings.youtube_video_dir
     expected_path = youtube_video_dir / "yModCU1OVHY.mp4"
     # remove the file if it exists
     expected_path.unlink(missing_ok=True)
 
     try:
-        youtube_video_input = YoutubeVideoInput(youtube_url=youtube_url)
-        video = download_youtube_video(youtube_video_input)
+        youtube_video_input = VideoInput(url=youtube_url)
+        video = download_video(youtube_video_input)
         assert isinstance(video, Video)
         assert video.path == expected_path
         assert video.path is not None
@@ -181,6 +192,6 @@ def test_download_youtube_video():
 
     # Test YoutubeVideoInput with invalid youtube_url
     youtube_url = "https://www.youtube.com/watch?v=invalid_url"
-    youtube_video_input = YoutubeVideoInput(youtube_url=youtube_url)
+    youtube_video_input = VideoInput(url=youtube_url)
     with pytest.raises(DownloadException):
-        download_youtube_video(youtube_video_input)
+        download_video(youtube_video_input)
