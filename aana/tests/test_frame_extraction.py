e988865f2a5146dac9daa0c55ab1895b8d12934a
movchan74 <aleksandr@mobius.ml>
2023-11-23 15:59:44 +0000

    Add tests for transcribe_stream and generate_frames_decord

--- aana/tests/test_frame_extraction.py
+++ aana/tests/test_frame_extraction.py
@@ -4,7 +4,7 @@ from aana.exceptions.general import VideoReadingException
 from aana.models.core.image import Image
 from aana.models.core.video import Video
 from aana.models.pydantic.video_params import VideoParams
-from aana.utils.video import extract_frames_decord
+from aana.utils.video import extract_frames_decord, generate_frames_decord
 
 
 @pytest.mark.parametrize(
@@ -33,9 +33,43 @@ def test_extract_frames_success(
     assert isinstance(result["frames"][0], Image)
     assert result["duration"] == expected_duration
     assert len(result["frames"]) == expected_num_frames
-    assert (
-        len(result["frames"]) == len(result["timestamps"]) - 1
-    )  # Minus 1 because the duration is added as the last timestamp
+    assert len(result["timestamps"]) == expected_num_frames
+
+
+@pytest.mark.parametrize(
+    "video_name, extract_fps, fast_mode_enabled, expected_duration, expected_num_frames",
+    [
+        ("squirrel.mp4", 1.0, False, 10.0, 10),
+        ("squirrel.mp4", 0.5, False, 10.0, 5),
+        ("squirrel.mp4", 1.0, True, 10.0, 4),
+    ],
+)
+def test_generate_frames_success(
+    video_name, extract_fps, fast_mode_enabled, expected_duration, expected_num_frames
+):
+    """
+    Test that generator generate_frames_decord can be used
+    to extract frames from a video successfully.
+    """
+
+    video_path = resources.path("aana.tests.files.videos", video_name)
+    video = Video(path=video_path)
+    params = VideoParams(extract_fps=extract_fps, fast_mode_enabled=fast_mode_enabled)
+    gen_frame = generate_frames_decord(video=video, params=params, batch_size=1)
+    total_frames = 0
+    for result in gen_frame:
+        assert "frames" in result
+        assert "timestamps" in result
+        assert "duration" in result
+        assert isinstance(result["duration"], float)
+        assert isinstance(result["frames"], list)
+        assert isinstance(result["frames"][0], Image)
+        assert result["duration"] == expected_duration
+        assert len(result["frames"]) == 1  # batch_size = 1
+        assert len(result["timestamps"]) == 1  # batch_size = 1
+        total_frames += 1
+
+    assert total_frames == expected_num_frames
 
 
 def test_extract_frames_failure():
