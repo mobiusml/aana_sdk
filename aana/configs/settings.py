b2925daef36a87af112b44c4d22b9b3683177a39
Evan de Riel <evan@mobius.ml>
2023-11-30 20:22:34 +0000

    Add models, repos, config

--- aana/configs/settings.py
+++ aana/configs/settings.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 from pydantic import BaseSettings
 
+from aana.configs.db import DBConfig
+
 
 class Settings(BaseSettings):
     """A pydantic model for SDK settings."""
@@ -9,6 +11,11 @@ class Settings(BaseSettings):
     tmp_data_dir: Path = Path("/tmp/aana_data")  # noqa: S108
     youtube_video_dir = tmp_data_dir / "youtube_videos"
     image_dir = tmp_data_dir / "images"
+
+    db_config: DBConfig = {
+        "datastore_type": "sqlite",
+        "datastore_config": {"path": Path("/var/lib/aana_data")},
+    }
 
 
 settings = Settings()
