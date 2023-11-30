d18be91200320e024639b034f7d22c38d7ff3a1b
Evan de Riel <evan@mobius.ml>
2023-11-30 20:22:58 +0000

    Add db util functions for saving

---
+++ aana/utils/db.py
@@ -0,0 +1,49 @@
+# ruff: noqa: A002
+from sqlalchemy.orm import Session
+
+from aana.configs.db import id_type
+from aana.models.db.caption import Caption
+from aana.models.db.media import Media, MediaType
+from aana.models.db.transcript import Transcript
+from aana.models.pydantic.asr_output import AsrTranscriptionList
+from aana.models.pydantic.captions import VideoCaptionsList
+from aana.repository.datastore.caption_repo import CaptionRepository
+from aana.repository.datastore.engine import engine
+from aana.repository.datastore.media_repo import MediaRepository
+from aana.repository.datastore.transcript_repo import TranscriptRepository
+
+
+# Just using raw utility functions like this isn't a permanent solution, but
+# it's good enough for now to validate what we're working on.
+def save_media(type: MediaType, duration: float) -> id_type:
+    """Creates and saves media to datastore.
+
+    Arguments:
+        type (MediaType): type of media
+        duration (float): duration of media
+
+    Returns:
+        id_type: datastore id of the inserted Media.
+    """
+    with Session(engine) as session:
+        media = Media(duration=duration, type=type)
+        repo = MediaRepository(session)
+        media = repo.create(media)
+        return media.id
+
+def save_captions(media_id: id_type, captions: VideoCaptionsList) -> list[id_type]:
+    """Save captions."""
+    with Session(engine) as session:
+        captions_ = [Caption(media_id=media_id, **c.dict()) for c in captions]
+        repo = CaptionRepository(session)
+        results = repo.create_multiple(captions_)
+        return [c.id for c in results]
+    
+
+def save_transcripts(media_id: id_type, transcripts: AsrTranscriptionList) -> list[id_type]:
+    """Save transcripts."""
+    with Session(engine) as session:
+        entities = [Transcript(media_id=media_id, **t.dict()) for t in transcripts]
+        repo = TranscriptRepository(session)
+        entities = repo.create_multiple(entities)
+        return [c.id for c in entities]
