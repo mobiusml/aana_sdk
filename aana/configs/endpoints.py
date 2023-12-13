8cbc4be75b624659356ebc9c6ebbb5e0284968ef
Evan de Riel <evan@mobius.ml>
2023-12-13 10:01:50 +0000

    Fix bug in blip2 video endpoint definition

--- aana/configs/endpoints.py
+++ aana/configs/endpoints.py
@@ -47,9 +47,9 @@ endpoints = {
             summary="Generate captions for videos using BLIP2 OPT-2.7B",
             outputs=[
                 EndpointOutput(
-                    name="captions", output="videos_captions_hf_blip2_opt_2_7b"
+                    name="captions", output="video_captions_hf_blip2_opt_2_7b"
                 ),
-                EndpointOutput(name="timestamps", output="timestamps"),
+                EndpointOutput(name="timestamps", output="video_timestamps"),
                 EndpointOutput(name="caption_ids", output="caption_ids"),
             ],
         ),
