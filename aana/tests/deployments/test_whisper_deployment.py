e7d75b54d6cceb21e74509c2a07fcd0b8e98a601
jiltseb <jiltsebastian@gmail.com>
2024-02-14 08:41:08 +0000

    added test scripts for vad, whisper deployments

--- aana/tests/deployments/test_whisper_deployment.py
+++ aana/tests/deployments/test_whisper_deployment.py
@@ -1,5 +1,6 @@
 # ruff: noqa: S101
 import json
+import os
 from collections import defaultdict
 from importlib import resources
 from pathlib import Path
@@ -7,7 +8,8 @@ from pathlib import Path
 import pytest
 from deepdiff import DeepDiff
 
-from aana.models.core.video import Video
+from aana.models.core.audio import Audio
+from aana.models.pydantic.vad_output import VadSegment
 from aana.models.pydantic.whisper_params import WhisperParams
 from aana.tests.utils import (
     LevenshteinOperator,
@@ -32,18 +34,17 @@ def compare_transcriptions(expected_transcription, transcription, words=True):
     Raises:
         AssertionError: if transcriptions differ too much
     """
-
     if words:
         levenshtein_operator = [LevenshteinOperator([r"\['text'\]$", r"\['word'\]$"])]
     else:
         levenshtein_operator = [LevenshteinOperator([r"\['text'\]$"])]
-    
+
     diff = DeepDiff(
         expected_transcription,
         transcription,
         math_epsilon=EPSILON,
         ignore_numeric_type_changes=True,
-        custom_operators= levenshtein_operator,
+        custom_operators=levenshtein_operator,
     )
     assert not diff, diff
 
@@ -60,15 +61,15 @@ def setup_whisper_deployment(setup_deployment, request):
     reason="GPU is not available",
 )
 @pytest.mark.asyncio
-@pytest.mark.parametrize("video_file", ["physicsworks.webm"])
-async def test_whisper_deployment(setup_whisper_deployment, video_file):
+@pytest.mark.parametrize("audio_file", ["physicsworks.wav"])
+async def test_whisper_deployment(setup_whisper_deployment, audio_file):
     """Test whisper deployment."""
     name, deployment, handle, port, route_prefix = setup_whisper_deployment
 
     model_size = deployment.user_config["model_size"]
-
+    audio_file_name = os.path.splitext(audio_file)[0]  # noqa: PTH122
     expected_output_path = resources.path(
-        f"aana.tests.files.expected.whisper.{model_size}", f"{video_file}.json"
+        f"aana.tests.files.expected.whisper.{model_size}", f"{audio_file_name}.json"
     )
     assert (
         expected_output_path.exists()
@@ -77,24 +78,20 @@ async def test_whisper_deployment(setup_whisper_deployment, video_file):
         expected_output = json.load(f)
 
     # Test transcribe method
-    path = resources.path("aana.tests.files.videos", video_file)
-    assert path.exists(), f"Video not found: {path}"
-    video = Video(path=path, media_id=video_file)
+    path = resources.path("aana.tests.files.audios", audio_file)
+    assert path.exists(), f"Audio not found: {path}"
+    audio = Audio(path=path, media_id=audio_file)
 
     output = await handle.transcribe.remote(
-        media=video, params=WhisperParams(word_timestamps=True)
+        media=audio, params=WhisperParams(word_timestamps=True)
     )
     output = pydantic_to_dict(output)
 
     compare_transcriptions(expected_output, output)
 
-    # Test transcribe_stream method
-    path = resources.path("aana.tests.files.videos", video_file)
-    assert path.exists(), f"Video not found: {path}"
-    video = Video(path=path, media_id=video_file)
-
+    # Test transcribe_stream method)
     stream = handle.options(stream=True).transcribe_stream.remote(
-        media=video, params=WhisperParams(word_timestamps=True)
+        media=audio, params=WhisperParams(word_timestamps=True)
     )
 
     # Combine individual segments and compare with the final dict
@@ -111,13 +108,63 @@ async def test_whisper_deployment(setup_whisper_deployment, video_file):
     compare_transcriptions(expected_output, dict(grouped_dict))
 
     # Test transcribe_batch method
-    videos = [video, video]
+    audios = [audio, audio]
 
     batch_output = await handle.transcribe_batch.remote(
-        media_batch=videos, params=WhisperParams(word_timestamps=True)
+        media_batch=audios, params=WhisperParams(word_timestamps=True)
     )
     batch_output = pydantic_to_dict(batch_output)
 
-    for i in range(len(videos)):
+    for i in range(len(audios)):
         output = {k: v[i] for k, v in batch_output.items()}
         compare_transcriptions(expected_output, output)
+
+    # Test batched_inference method: Note that the expected asr output is different
+    expectd_batched_output_path = resources.path(
+        f"aana.tests.files.expected.whisper.{model_size}",
+        f"{audio_file_name}_batched.json",
+    )
+    assert (
+        expectd_batched_output_path.exists()
+    ), f"Expected output not found: {expectd_batched_output_path}"
+    with Path(expectd_batched_output_path) as path, path.open() as f:
+        expected_output_batched = json.load(f)
+
+    # get expected vad_segments
+    vad_path = resources.path(
+        "aana.tests.files.expected.vad", f"{audio_file_name}_vad.json"
+    )
+    assert vad_path.exists(), f"vad expected predictions not found: {vad_path}"
+
+    with Path(vad_path) as path, path.open() as f:
+        expected_output_vad = json.load(f)
+
+    final_input = []
+    for seg in expected_output_vad["vad_segments"]:
+        final_input.append(  # noqa: PERF401
+            VadSegment(time_interval=seg["time_interval"], segments=seg["segments"])
+        )
+
+    batched_stream = handle.options(stream=True).batched_inference.remote(
+        media=audio,  # chnge all
+        vad_segments=final_input,
+        batch_size=16,
+        params=WhisperParams(word_timestamps=False),
+    )
+
+    # Combine individual segments and compare with the final dict
+    transcript = ""
+    grouped_dict = defaultdict(list)
+    async for chunk in batched_stream:
+        chunk = await chunk
+        output = pydantic_to_dict(chunk)
+        transcript += output["transcription"]["text"]
+        grouped_dict["segments"].append(output.get("segments")[0])
+
+    grouped_dict["transcription"] = {"text": transcript}
+    grouped_dict["transcription_info"] = output.get("transcription_info")
+    compare_transcriptions(
+        expected_output_batched,
+        dict(grouped_dict),
+        words=False,
+    )
