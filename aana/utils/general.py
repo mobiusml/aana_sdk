d8ddaae9dee5de90039b0e41904c051c4cc12dcc
jiltseb <jiltsebastian@gmail.com>
2024-02-27 14:29:04 +0000

    added download function

--- aana/utils/general.py
+++ aana/utils/general.py
@@ -1,8 +1,13 @@
 import hashlib
+import urllib.request
+from contextlib import ExitStack
+from pathlib import Path
 from typing import Any, TypeVar
 
 import requests
+import torch
 from pydantic import BaseModel
+from tqdm import tqdm
 
 from aana.api.api_generation import Endpoint
 from aana.configs.endpoints import endpoints as all_endpoints
@@ -33,6 +38,64 @@ def merged_options(default_options: OptionType, options: OptionType) -> OptionTy
         if v is not None:
             default_options_dict[k] = v
     return options.__class__.parse_obj(default_options_dict)
+
+
+# model download from a url and cheking SHA sum of the URL.
+# TODO: To modify the download function to download model from HF as well.
+def download_model(url: str, model_path: Path | None = None) -> Path:
+    """Download a model from a URL.
+
+    Args:
+        url (str): the URL of the file to download
+        model_path (Path): optional model path where it needs to be downloaded
+
+    Returns:
+        Path: the downloaded file path
+
+    Raises:
+        DownloadException: Request does not succeed.
+    """
+    if model_path is None:
+        model_dir = torch.hub._get_torch_home()
+        if not Path(model_dir).exists():
+            Path(model_dir).mkdir(parents=True)
+        model_path = Path(model_dir) / "pytorch_model.bin"
+
+    if Path(model_path).exists() and not Path(model_path).is_file():
+        raise RuntimeError(f"{model_path}")  # exists and is not a regular file
+
+    if not Path(model_path).is_file():
+        try:
+            with ExitStack() as stack:
+                source = stack.enter_context(urllib.request.urlopen(url))  # noqa: S310
+                output = stack.enter_context(Path.open(model_path, "wb"))
+
+                loop = tqdm(
+                    total=int(source.info().get("Content-Length")),
+                    ncols=80,
+                    unit="iB",
+                    unit_scale=True,
+                    unit_divisor=1024,
+                )
+
+                with loop:
+                    while True:
+                        buffer = source.read(8192)
+                        if not buffer:
+                            break
+
+                        output.write(buffer)
+                        loop.update(len(buffer))
+        except Exception as e:
+            raise DownloadException(url) from e
+
+    model_bytes = Path.open(model_path, "rb").read()
+
+    if hashlib.sha256(model_bytes).hexdigest() != str(url).split("/")[-2]:
+        checksum_error = "Model has been downloaded but the SHA256 checksum does not not match. Please retry loading the model."
+        raise RuntimeError(f"{checksum_error}")
+
+    return Path(model_path)
 
 
 def download_file(url: str) -> bytes:
