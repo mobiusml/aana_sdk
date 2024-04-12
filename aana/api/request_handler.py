d1ad04bb22749bcb3c94dc8832590a116a4d7ea5
Aleksandr Movchan <movchan174@gmail.com>
2024-04-12 14:34:21 +0000

    Started rewrite

--- aana/api/request_handler.py
+++ aana/api/request_handler.py
@@ -1,45 +1,31 @@
 from typing import Any
 
 from fastapi.openapi.utils import get_openapi
-from mobius_pipeline.pipeline import Pipeline
 from ray import serve
 
 from aana.api.api_generation import Endpoint, add_custom_schemas_to_openapi_schema
 from aana.api.app import app
 from aana.api.responses import AanaJSONResponse
 
-# TODO: improve type annotations
 
-
-@serve.deployment(ray_actor_options={"num_cpus": 0.1})
+@serve.deployment(route_prefix="/", num_replicas=1, ray_actor_options={"num_cpus": 0.1})
 @serve.ingress(app)
 class RequestHandler:
     """This class is used to handle requests to the Aana application."""
 
     ready = False
 
-    def __init__(
-        self,
-        endpoints: list[Endpoint],
-        pipeline_nodes: list[dict[str, Any]],
-        context: dict[str, Any],
-    ):
+    def __init__(self, endpoints: list[Endpoint]):
         """Constructor.
 
         Args:
             endpoints (dict): List of endpoints for the request
-            pipeline_nodes (list[dict[str, Any]]]): List of nodes for the pipeline
-            context (dict): Pipeline context so pipeline can use  deployment handles.
         """
-        self.context = context
         self.endpoints = endpoints
-        self.pipeline = Pipeline(pipeline_nodes, context)
 
         self.custom_schemas: dict[str, dict] = {}
         for endpoint in self.endpoints:
-            endpoint.register(
-                app=app, pipeline=self.pipeline, custom_schemas=self.custom_schemas
-            )
+            endpoint.register(app=app, custom_schemas=self.custom_schemas)
 
         app.openapi = self.custom_openapi
         self.ready = True
@@ -55,14 +41,6 @@ class RequestHandler:
         )
         app.openapi_schema = openapi_schema
         return app.openapi_schema
-
-    def get_context(self) -> dict:
-        """Getter for pipeline context.
-
-        Returns:
-            dict: The context of the pipeline.
-        """
-        return self.context
 
     @app.get("/api/ready")
     async def is_ready(self):
