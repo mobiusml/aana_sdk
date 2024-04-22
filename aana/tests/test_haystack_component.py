fe300ed8b54a8e8c2d5b3fa11c1d095eb15a1676
evanderiel <evanrderiel@gmail.com>
2024-04-22 14:56:35 +0000

    Fixes

--- aana/tests/test_haystack_component.py
+++ aana/tests/test_haystack_component.py
@@ -1,12 +1,12 @@
 import pytest
 
-from aana.components.deployment_component import DeploymentComponent
+from aana.components.deployment_component import AanaDeploymentComponent
 from aana.configs.deployments import deployments
 
 
-@pytest.fixture("deployment_name", ["stablediffusion2_deployment"])
+@pytest.mark.parametrize("deployment_name", ["stablediffusion2_deployment"])
 def test_haystack_wrapper(deployment_name):
     """Tests haystack wrapper for deployments."""
-    component = DeploymentComponent(deployments[deployment_name])
+    component = AanaDeploymentComponent(deployments[deployment_name])
     component.warm_up()
     # TODO: run functionality
