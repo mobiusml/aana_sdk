d8df7df3c6f62a1bfbc096859f8a86eb47add704
Aleksandr Movchan <movchan174@gmail.com>
2024-04-23 11:28:53 +0000

    Removed BaseStringModel, cleaned up code and README, changed version to 0.2.

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # TODO: pass arguments to the docker to set target instead of environment variable
-HF_HUB_ENABLE_HF_TRANSFER=1 poetry run aana --port 8000 --host 0.0.0.0 --target $TARGET
+HF_HUB_ENABLE_HF_TRANSFER=1  poetry run python aana/projects/$TARGET/app.py --port 8000 --host 0.0.0.0
