35726a246dda493034825e9828c3f35ffd9acfba
evanderiel <evanrderiel@gmail.com>
2024-04-25 11:47:05 +0200

    Update deployment component

--- aana/components/deployment_component.py
+++ aana/components/deployment_component.py
@@ -4,13 +4,12 @@ from typing import Any, TypeAlias, get_type_hints
 
 import ray
 from haystack import component
-from pydantic import BaseModel
-from ray.serve.deployment import Deployment
 
 from aana.utils.coroutines import run_sync
 from aana.utils.typing import as_dict_of_types, is_typed_dict
 
 DeploymentResult: TypeAlias = Any
+AanaDeploymentHandle: TypeAlias = Any
 
 
 def typehints_to_component_types(
@@ -59,7 +58,7 @@ def typehints_to_input_types(typehints: dict[str]) -> dict[str]:
         typehints (dict[str, type]): Type hints from `typing.get_type_hints()` *minus the "return" key*
 
     Returns:
-        dict[str, type]: Something that can be consumed by `haystack.set_input_types()`
+                dict[str, type]: Something that can be consumed by `haystack.set_input_types()`
     """
     # If typehint is None, or an emtpy dict, return an empty dict
     if not typehints:
@@ -75,31 +74,27 @@ class AanaDeploymentComponent:
 
     # TODO: add usage example to docstring
 
-    _deployment: Deployment
-    _config: BaseModel
+    _deployment: AanaDeploymentHandle
     _inference_method: Callable
     _warm: bool
 
-    def __init__(self, deployment: Deployment, method_name="generate_batch"):
+    def __init__(
+        self, deployment_handle: AanaDeploymentHandle, method_name="generate_batch"
+    ):
         """Constructor.
 
         Arguments:
-            deployment (Deployment): the Ray deployment to be wrapped
-            config (BaseModel): the config for the deploy to set in its `apply_config()` method.
+            deployment_handle (AanaDeploymentHandle): the Ray deployment to be wrapped
             method_name (str): the name of the method on the deployment to call inside the component's `run()` method. Defaults to `generate_batch`
         """
-        self._deployment = deployment
-        self.config = deployment.user_config
-        self._warm = False
-        self._method_name = method_name
-        self._serve_handle = None
+        self._deployment = deployment_handle
         self._run_method = None
 
         # Determine input and output types for `run()`
-        method_handle = self._get_method()
-        if not method_handle:
-            raise ValueError(self._method_name)
-        hints = get_type_hints(method_handle)
+        self.run_method = self._get_method(method_name)
+        if not self.run_method:
+            raise ValueError(method_name)
+        hints = get_type_hints(self.run_method)
         input_types, output_types = typehints_to_component_types(hints)
         # The functions `set_input_types()` and `set_output_types()`
         # take an positionial instance argument and keyword arguments
@@ -109,22 +104,9 @@ class AanaDeploymentComponent:
     def warm_up(self):
         """Warms up the deployment to a ready state.
 
-        Usually this is to load the model and initialize any preallocated data or parameters.
+        As we run off an existing deployment handle, this is currently a no-op.
         """
-        # TODO possble race condition on self._warm
-        if not self._warm:
-            self._warm = True
-
-            # Start ray if it isn't already
-            # @movchan74 Do we do this here or elsewhere?
-            ray.init(ignore_reinit_error=True)
-            # Run the deployment
-            self._serve_handle = ray.serve.run(self._deployment.bind())
-            self._run_method = getattr(self._serve_handle, self._method_name, None)
-            if not self._run_method:
-                # Should not happen but just in case
-                raise ValueError(self._method_name)
-            # self._serve_handle.apply_config(self._config)
+        pass
 
     def run(self, *args, **kwargs) -> DeploymentResult:
         """Run the component. This is the primary interface for Haystack Components.
@@ -148,9 +130,9 @@ class AanaDeploymentComponent:
         """Calls the deployment's run method. Not public, use the `run()` method."""
         return self._run_method(*args, **kwargs).remote()  # type: ignore
 
-    def _get_method(self) -> Callable | None:
+    def _get_method(self, method_name: str) -> Callable | None:
         """Gets a handle to the method specified by the constructor."""
-        # This is the type/constructor of the deployment
-        underlying_deployment_t = self._deployment.func_or_class
-        class_method_handle = getattr(underlying_deployment_t, self._method_name, None)
-        return class_method_handle  # type: ignore
+        instance_method_handle: Callable | None = getattr(
+            self._deployment_handle, method_name, None
+        )
+        return instance_method_handle
