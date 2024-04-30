6c33b2609e5773f4951b7336da1040b594502edb
Aleksandr Movchan <movchan174@gmail.com>
2024-04-30 14:10:56 +0000

    Better errors for deploy. Fixed Ray Serve shutdown. Refactor API generation code to handle missing return type annotations.

--- aana/configs/deployments.py
+++ aana/configs/deployments.py
@@ -18,7 +18,7 @@ available_deployments = {}
 
 vllm_llama2_7b_chat_deployment = VLLMDeployment.options(
     num_replicas=1,
-    max_concurrent_queries=1000,
+    max_ongoing_requests=1000,
     ray_actor_options={"num_gpus": 0.25},
     user_config=VLLMConfig(
         model="TheBloke/Llama-2-7b-Chat-AWQ",
@@ -30,48 +30,48 @@ vllm_llama2_7b_chat_deployment = VLLMDeployment.options(
             temperature=0.0, top_p=1.0, top_k=-1, max_tokens=1024
         ),
         chat_template="llama2",
-    ).model_dump(),
+    ).model_dump(mode="json"),
 )
 available_deployments["vllm_llama2_7b_chat_deployment"] = vllm_llama2_7b_chat_deployment
 
 hf_blip2_opt_2_7b_deployment = HFBlip2Deployment.options(
     num_replicas=1,
-    max_concurrent_queries=1000,
+    max_ongoing_requests=1000,
     ray_actor_options={"num_gpus": 0.25},
     user_config=HFBlip2Config(
         model="Salesforce/blip2-opt-2.7b",
         dtype=Dtype.FLOAT16,
         batch_size=2,
         num_processing_threads=2,
-    ).model_dump(),
+    ).model_dump(mode="json"),
 )
 available_deployments["hf_blip2_opt_2_7b_deployment"] = hf_blip2_opt_2_7b_deployment
 
 whisper_medium_deployment = WhisperDeployment.options(
     num_replicas=1,
-    max_concurrent_queries=1000,
+    max_ongoing_requests=1000,
     ray_actor_options={"num_gpus": 0.25},
     user_config=WhisperConfig(
         model_size=WhisperModelSize.MEDIUM,
         compute_type=WhisperComputeType.FLOAT16,
-    ).model_dump(),
+    ).model_dump(mode="json"),
 )
 available_deployments["whisper_medium_deployment"] = whisper_medium_deployment
 
 stablediffusion2_deployment = StableDiffusion2Deployment.options(
     num_replicas=1,
-    max_concurrent_queries=1000,
+    max_ongoing_requests=1000,
     ray_actor_options={"num_gpus": 1},
     user_config=StableDiffusion2Config(
         model="stabilityai/stable-diffusion-2",
         dtype=Dtype.FLOAT16,
-    ).model_dump(),
+    ).model_dump(mode="json"),
 )
 available_deployments["stablediffusion2_deployment"] = stablediffusion2_deployment
 
 vad_deployment = VadDeployment.options(
     num_replicas=1,
-    max_concurrent_queries=1000,
+    max_ongoing_requests=1000,
     ray_actor_options={"num_gpus": 0.05},
     user_config=VadConfig(
         model=(
@@ -80,6 +80,6 @@ vad_deployment = VadDeployment.options(
         ),
         onset=0.5,
         sample_rate=16000,
-    ).model_dump(),
+    ).model_dump(mode="json"),
 )
 available_deployments["vad_deployment"] = vad_deployment
