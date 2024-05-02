55b95e2c50af607fb2bc68112e210cefadf8fcbb
Aleksandr Movchan <movchan174@gmail.com>
2024-05-02 15:23:24 +0000

    Refactoring for AanaSDK class and build functionality

--- aana/projects/whisper/app.py
+++ aana/projects/whisper/app.py
@@ -1,4 +1,5 @@
 import argparse
+from pathlib import Path
 
 from aana.configs.deployments import vad_deployment, whisper_medium_deployment
 from aana.projects.whisper.endpoints import (
@@ -47,28 +48,70 @@ endpoints = [
     },
 ]
 
+aana_app = AanaSDK(name="whisper_app")
 
-if __name__ == "__main__":
-    """Runs the application."""
+for deployment in deployments:
+    aana_app.register_deployment(
+        name=deployment["name"],
+        instance=deployment["instance"],
+    )
+
+for endpoint in endpoints:
+    aana_app.register_endpoint(
+        name=endpoint["name"],
+        path=endpoint["path"],
+        summary=endpoint["summary"],
+        endpoint_cls=endpoint["endpoint_cls"],
+    )
+
+
+def main():
+    """Main function to run the application."""
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("--port", type=int, default=8000)
-    arg_parser.add_argument("--host", type=str, default="127.0.0.1")
+    subparsers = arg_parser.add_subparsers(dest="command", help="commands")
+
+    # Deploy command
+    deploy_parser = subparsers.add_parser("deploy", help="Deploy the application")
+    deploy_parser.add_argument(
+        "--port", type=int, default=8000, help="Port to run the application"
+    )
+    deploy_parser.add_argument(
+        "--host", type=str, default="127.0.0.1", help="Host address"
+    )
+
+    # Build command
+    build_parser = subparsers.add_parser("build", help="Build the application")
+    build_parser.add_argument(
+        "--import_path", type=str, help="Import path", required=True
+    )
+    build_parser.add_argument(
+        "--host",
+        type=str,
+        default="0.0.0.0",  # noqa: S104
+        help="Host address",
+    )
+    build_parser.add_argument(
+        "--port", type=int, default=8000, help="Port to run the application"
+    )
+    build_parser.add_argument(
+        "--output_dir", type=str, help="Output directory", default=Path(__file__).parent
+    )
+
     args = arg_parser.parse_args()
 
-    aana_app = AanaSDK(port=args.port, host=args.host, show_logs=True)
-
-    for deployment in deployments:
-        aana_app.register_deployment(
-            name=deployment["name"],
-            instance=deployment["instance"],
+    if args.command == "deploy":
+        aana_app.connect(port=args.port, host=args.host, show_logs=True)
+        aana_app.deploy(blocking=True)
+    elif args.command == "build":
+        aana_app.build(
+            import_path=args.import_path,
+            host=args.host,
+            port=args.port,
+            output_dir=args.output_dir,
         )
+    else:
+        arg_parser.print_help()
 
-    for endpoint in endpoints:
-        aana_app.register_endpoint(
-            name=endpoint["name"],
-            path=endpoint["path"],
-            summary=endpoint["summary"],
-            endpoint_cls=endpoint["endpoint_cls"],
-        )
 
-    aana_app.deploy(blocking=True)
+if __name__ == "__main__":
+    main()
