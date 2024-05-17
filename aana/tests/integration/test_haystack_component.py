ab5885f6ef21d668d3b012f869cf4d54005714e3
evanderiel <evanrderiel@gmail.com>
2024-05-17 09:16:07 +0000

    Make tests async

--- aana/tests/integration/test_haystack_component.py
+++ aana/tests/integration/test_haystack_component.py
@@ -10,19 +10,19 @@ from aana.tests.deployments.test_stablediffusion2_deployment import (
     setup_deployment as setup_stablediffusion2_deployment,  # noqa: F401
 )
 from aana.tests.utils import is_gpu_available, is_using_deployment_cache
-from aana.utils.asyncio import run_async
 
 
 @pytest.mark.skipif(
     not is_gpu_available() and not is_using_deployment_cache(),
     reason="GPU is not available",
 )
-def test_haystack_wrapper(setup_stablediffusion2_deployment):  # noqa: F811
+@pytest.mark.asyncio
+async def test_haystack_wrapper(setup_stablediffusion2_deployment):  # noqa: F811
     """Tests haystack wrapper for deployments."""
     deployment_name = "sd2_deployment"
     method_name = "generate"
     result_key = "image"
-    deployment_handle = run_async(AanaDeploymentHandle.create(deployment_name))
+    deployment_handle = await AanaDeploymentHandle.create(deployment_name)
     component = AanaDeploymentComponent(deployment_handle, method_name)
     result = component.run(prompt="foo")
     assert result_key in result, result
@@ -79,10 +79,11 @@ async def test_haystack_pipeline(setup_stablediffusion2_deployment):  # noqa: F8
     assert len(result["image_sizer"]["size"]) == 2
 
 
-def test_haystack_wrapper_fails(setup_stablediffusion2_deployment):  # noqa: F811
+@pytest.mark.asyncio
+async def test_haystack_wrapper_fails(setup_stablediffusion2_deployment):  # noqa: F811
     """Tests that haystack wrapper raises if method_name is missing."""
     deployment_name = "sd2_deployment"
     missing_method_name = "does_not_exist"
-    deployment_handle = run_async(AanaDeploymentHandle.create(deployment_name))
+    deployment_handle = await AanaDeploymentHandle.create(deployment_name)
     with pytest.raises(AttributeError):
         _component = AanaDeploymentComponent(deployment_handle, missing_method_name)
