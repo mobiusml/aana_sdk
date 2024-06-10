55343e7b8abee00f9e3b1068fe2f6a46f124d6ac
Aleksandr Movchan <movchan174@gmail.com>
2024-06-10 13:25:09 +0000

    Requested PR changes.

--- aana/storage/services/task.py
+++ aana/storage/services/task.py
@@ -1,26 +1,13 @@
+from datetime import datetime
 from typing import Any
 
-from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 
+from aana.core.models.task import TaskInfo
 from aana.storage.engine import engine
 from aana.storage.models.task import Status as TaskStatus
 from aana.storage.models.task import TaskEntity
 from aana.storage.repository.task import TaskRepository
-
-
-class TaskInfo(BaseModel):
-    """Task information.
-
-    Attributes:
-        id (str): The task ID.
-        status (TaskStatus): The task status.
-        result (Any): The task result.
-    """
-
-    id: str = Field(..., description="The task ID.")
-    status: TaskStatus = Field(..., description="The task status.")
-    result: Any = Field(None, description="The task result.")
 
 
 def create_task(
@@ -65,3 +52,58 @@ def get_task_info(task_id: str) -> TaskInfo:
             status=task.status,
             result=task.result,
         )
+
+
+def get_task(task_id: str) -> TaskEntity:
+    """Get a task.
+
+    Args:
+        task_id (str): The task ID.
+
+    Returns:
+        TaskEntity: The task.
+    """
+    with Session(engine) as session:
+        task_repo = TaskRepository(session)
+        task = task_repo.read(task_id)
+        return task
+
+
+def update_task_status(
+    task_id: str, status: TaskStatus, progress: int | None = None, result: Any = None
+):
+    """Update the task status.
+
+    Args:
+        task_id (str): The task ID.
+        status (TaskStatus): The new status.
+        progress (int | None): The progress. If None, the progress will not be updated.
+        result (Any): The result.
+    """
+    with Session(engine) as session:
+        task_repo = TaskRepository(session)
+        task = task_repo.read(task_id)
+        task.status = status
+        task.completed_at = datetime.now()  # noqa: DTZ005
+        if progress is not None:
+            task.progress = progress
+        task.result = result
+        session.commit()
+
+
+def get_unprocessed_tasks(limit: int | None = None) -> list[TaskEntity]:
+    """Fetches all unprocessed tasks.
+
+    The task is considered unprocessed if it is in CREATED state or
+    in RUNNING or ASSIGNED state and the update timestamp is older
+    than the execution timeout (to handle stuck tasks).
+
+    Args:
+        limit (int | None): The maximum number of tasks to fetch. If None, fetch all.
+
+    Returns:
+        list[TaskEntity]: the unprocessed tasks.
+    """
+    with Session(engine) as session:
+        task_repo = TaskRepository(session)
+        return task_repo.get_unprocessed_tasks(limit)
