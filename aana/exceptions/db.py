d932b609e906915c67edc9128963006fd57111ee
Hossein Rashidi <h.rashidi128@gmail.com>
2024-06-20 11:20:07 +0200

    add status for video exception

--- aana/exceptions/db.py
+++ aana/exceptions/db.py
@@ -1,5 +1,6 @@
 from aana.core.models.media import MediaId
 from aana.exceptions.core import BaseException
+from aana.storage.models.video import Status as VideoStatus
 
 
 class NotFoundException(BaseException):
@@ -45,21 +46,24 @@ class UnfinishedVideoException(BaseException):
     """Exception raised when try to fetch unfinished video.
 
     Attributes:
-        media_id (int | MediaId): the id of the item to be retrieved.
+        media_id (int | MediaId): The id of video.
+        status (VideoStatus): The current video status.
         message (str): The error message.
     """
 
-    def __init__(self, media_id: int | MediaId, message: str):
+    def __init__(self, media_id: int | MediaId, status: VideoStatus, message: str):
         """Constructor.
 
         Args:
-            media_id (int | MediaId): the id of the item to be retrieved.
+            media_id (int | MediaId): The id of video.
+            status (VideoStatus): The current video status.
             message (str): The error message.
         """
-        super().__init__(media_id=media_id, message=message)
+        super().__init__(media_id=media_id, status=status, message=message)
         self.media_id = media_id
+        self.status = status
         self.message = message
 
     def __reduce__(self):
         """Used for pickling."""
-        return (self.__class__, (self.media_id, self.message))
+        return (self.__class__, (self.media_id, self.status, self.message))
