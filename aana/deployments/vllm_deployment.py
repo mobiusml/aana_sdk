8ae81c7bdd5ed41af0dd9d6b83fbbfd1e0bf173c
Hossein Rashidi <h.rashidi128@gmail.com>
2024-07-04 00:24:32 +0200

    add new vllm model

--- aana/deployments/vllm_deployment.py
+++ aana/deployments/vllm_deployment.py
@@ -8,6 +8,7 @@ from vllm.engine.arg_utils import AsyncEngineArgs
 from vllm.engine.async_llm_engine import AsyncLLMEngine
 
 from aana.core.models.base import merged_options
+from aana.core.models.custom_config import CustomConfig
 from aana.deployments.base_text_generation_deployment import (
     BaseTextGenerationDeployment,
     LLMOutput,
@@ -38,6 +39,9 @@ class VLLMConfig(BaseModel):
         max_model_len (int): the maximum generated text length in tokens (optional, default: None)
         chat_template (str): the name of the chat template, if not provided, the chat template from the model will be used
                              but some models may not have a chat template (optional, default: None)
+        enforce_eager: whether to enforce eager execution (optional, default: False)
+        engine_args: extra engine arguments (optional, default: {})       
+
     """
 
     model: str
@@ -50,7 +54,7 @@ class VLLMConfig(BaseModel):
     max_model_len: int | None = Field(default=None)
     chat_template: str | None = Field(default=None)
     enforce_eager: bool | None = Field(default=False)
-
+    engine_args: CustomConfig = {}
 
 @serve.deployment
 class VLLMDeployment(BaseTextGenerationDeployment):
@@ -95,6 +99,7 @@ class VLLMDeployment(BaseTextGenerationDeployment):
             enforce_eager=config_obj.enforce_eager,
             gpu_memory_utilization=self.gpu_memory_utilization,
             max_model_len=config_obj.max_model_len,
+            **config_obj.engine_args
         )
 
         # TODO: check if the model is already loaded.
