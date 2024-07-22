8a2ef85dbe32ad2fcd1e498a8f89a4a0c81a4ceb
Aleksandr Movchan <aleksandr@mobiuslabs.com>
2024-07-22 11:42:31 +0000

    Added more models to the docs

--- aana/core/models/captions.py
+++ aana/core/models/captions.py
@@ -2,6 +2,14 @@ from typing import Annotated
 
 from pydantic import Field
 
+__all__ = ["Caption", "CaptionsList"]
+
 Caption = Annotated[str, Field(description="A caption.")]
+"""
+A caption.
+"""
 
 CaptionsList = Annotated[list[Caption], Field(description="A list of captions.")]
+"""
+A list of captions.
+"""
