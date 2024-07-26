89cab2674a3975dd4a4fe4fce5cbf868a8fe523a
Aleksandr Movchan <aleksandr@mobiuslabs.com>
2024-07-26 09:04:10 +0000

    Removed chat with video app

--- aana/exceptions/db.py
+++ aana/exceptions/db.py
@@ -1,6 +1,5 @@
 from aana.core.models.media import MediaId
 from aana.exceptions.core import BaseException
-from aana.storage.models.extended_video import VideoProcessingStatus
 
 
 class NotFoundException(BaseException):
@@ -40,32 +39,3 @@ class MediaIdAlreadyExistsException(BaseException):
     def __reduce__(self):
         """Used for pickling."""
         return (self.__class__, (self.table_name, self.media_id))
-
-
-class UnfinishedVideoException(BaseException):
-    """Exception raised when try to fetch unfinished video.
-
-    Attributes:
-        media_id (int | MediaId): The id of video.
-        status (VideoStatus): The current video status.
-        message (str): The error message.
-    """
-
-    def __init__(
-        self, media_id: int | MediaId, status: VideoProcessingStatus, message: str
-    ):
-        """Constructor.
-
-        Args:
-            media_id (int | MediaId): The id of video.
-            status (VideoStatus): The current video status.
-            message (str): The error message.
-        """
-        super().__init__(media_id=media_id, status=status, message=message)
-        self.media_id = media_id
-        self.status = status
-        self.message = message
-
-    def __reduce__(self):
-        """Used for pickling."""
-        return (self.__class__, (self.media_id, self.status, self.message))
