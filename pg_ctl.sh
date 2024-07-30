bfbba29809bda3f4c8940501065f5bb7ae8a495e
Hossein Rashidi <h.rashidi128@gmail.com>
2024-07-30 13:26:07 +0200

    format pg_ctl

--- pg_ctl.sh
+++ pg_ctl.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$2" = "--pgdata" ]; then
-	# Recursively change parent directories permissions so that pg_ctl can access the necessary files as non-root.
-	f=$(dirname "$3")
-	while [[ $f != / ]]; do chmod o+rwx "$f"; f=$(dirname "$f"); done;
+    # Recursively change parent directories permissions so that pg_ctl can access the necessary files as non-root.
+    f=$(dirname "$3")
+    while [[ $f != / ]]; do chmod o+rwx "$f"; f=$(dirname "$f"); done;
 fi
 
 # Execute pg_ctl as postgres instead of root.
