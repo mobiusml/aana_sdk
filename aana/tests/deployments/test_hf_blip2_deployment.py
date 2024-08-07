1c85ea9a46b1c514dbd461bdbc93fb07ab72eb06
Aleksandr Movchan <aleksandr@mobiuslabs.com>
2024-08-07 08:15:22 +0000

    Updated HF BLIP2 and HF Pipeline tests. Removed stable diffusion.

--- aana/tests/deployments/test_hf_blip2_deployment.py
+++ aana/tests/deployments/test_hf_blip2_deployment.py
@@ -2,63 +2,64 @@
 from importlib import resources
 
 import pytest
-from ray import serve
 
 from aana.core.models.image import Image
+from aana.core.models.types import Dtype
+from aana.deployments.aana_deployment_handle import AanaDeploymentHandle
+from aana.deployments.hf_blip2_deployment import HFBlip2Config, HFBlip2Deployment
 from aana.tests.utils import (
-    compare_texts,
-    get_deployments_by_type,
     is_gpu_available,
+    verify_deployment_results,
 )
 
+deployments = [
+    (
+        "blip2_deployment",
+        HFBlip2Deployment.options(
+            num_replicas=1,
+            max_ongoing_requests=1000,
+            ray_actor_options={"num_gpus": 0.25},
+            user_config=HFBlip2Config(
+                model="Salesforce/blip2-opt-2.7b",
+                dtype=Dtype.FLOAT16,
+                batch_size=2,
+                num_processing_threads=2,
+            ).model_dump(mode="json"),
+        ),
+    )
+]
 
-@pytest.fixture(scope="function", params=get_deployments_by_type("HFBlip2Deployment"))
-def setup_hf_blip2_deployment(create_app, request):
-    """Setup HF BLIP2 deployment."""
-    name, deployment = request.param
-    deployments = [
-        {
-            "name": "blip2_deployment",
-            "instance": deployment,
-        }
-    ]
-    endpoints = []
 
-    return name, deployment, create_app(deployments, endpoints)
-
-
-@pytest.mark.skipif(
-    not is_gpu_available(),
-    reason="GPU is not available",
-)
+@pytest.mark.skipif(not is_gpu_available(), reason="GPU is not available")
 @pytest.mark.asyncio
-@pytest.mark.parametrize(
-    "image_name, expected_text",
-    [
-        (
-            "Starry_Night.jpeg",
-            "the starry night by vincent van gogh, 1884-1890, oil on canvas, 48 x 48 in, gilded frame, signed and dated",
-        )
-    ],
-)
+@pytest.mark.parametrize("deployment_name, deployment", deployments)
+@pytest.mark.parametrize("image_name", ["Starry_Night.jpeg"])
 async def test_hf_blip2_deployments(
-    setup_hf_blip2_deployment, image_name, expected_text
+    setup_deployment, deployment_name, deployment, image_name
 ):
     """Test HuggingFace BLIP2 deployments."""
-    handle = serve.get_app_handle("blip2_deployment")
+    setup_deployment(deployment_name, deployment)
+
+    handle = await AanaDeploymentHandle.create(deployment_name)
+
+    expected_output_path = (
+        resources.path("aana.tests.files.expected", "")
+        / "hf_blip2"
+        / f"{deployment_name}_{image_name}.json"
+    )
 
     path = resources.path("aana.tests.files.images", image_name)
     image = Image(path=path, save_on_disk=False, media_id=image_name)
 
-    output = await handle.generate.remote(image=image)
+    output = await handle.generate(image=image)
     caption = output["caption"]
-    compare_texts(expected_text, caption)
+    verify_deployment_results(expected_output_path, caption)
 
     images = [image] * 8
 
-    output = await handle.generate_batch.remote(images=images)
+    output = await handle.generate_batch(images=images)
     captions = output["captions"]
 
     assert len(captions) == 8
     for caption in captions:
-        compare_texts(expected_text, caption)
+        verify_deployment_results(expected_output_path, caption)
