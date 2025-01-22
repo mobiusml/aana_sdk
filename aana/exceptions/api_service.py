e08fc2beadb79c22b7a798cc1e9d61ea711c972e
Aleksandr Movchan <aleksandr@mobiuslabs.com>
2025-01-22 11:45:38 +0000

    Implement API key validation middleware and integrate LAGO API service for usage tracking

---
+++ aana/exceptions/api_service.py
@@ -0,0 +1,60 @@
+from aana.exceptions.core import BaseException
+
+
+class ApiKeyNotProvided(BaseException):
+    """Exception raised when the API key is not provided."""
+
+    def __init__(self):
+        """Initialize the exception."""
+        self.message = "API key not provided"
+        super().__init__(message=self.message)
+
+    def __reduce__(self):
+        """Used for pickling."""
+        return (self.__class__, ())
+
+
+class ApiKeyNotFound(BaseException):
+    """Exception raised when the API key is not found.
+
+    Attributes:
+        key (str): the API key that was not found
+    """
+
+    def __init__(self, key: str):
+        """Initialize the exception.
+
+        Args:
+            key (str): the API key that was not found
+        """
+        self.key = key
+        self.message = f"API key {key} not found"
+        super().__init__(key=key, message=self.message)
+
+    def __reduce__(self):
+        """Used for pickling."""
+        return (self.__class__, (self.key,))
+
+
+class InactiveSubscription(BaseException):
+    """Exception raised when the subscription is inactive (e.g. credits are not available).
+
+    Attributes:
+        key (str): the API key with inactive subscription
+    """
+
+    def __init__(self, key: str):
+        """Initialize the exception.
+
+        Args:
+            key (str): the API key with inactive subscription
+        """
+        self.key = key
+        self.message = (
+            f"API key {key} has an inactive subscription. Check your credits."
+        )
+        super().__init__(key=key, message=self.message)
+
+    def __reduce__(self):
+        """Used for pickling."""
+        return (self.__class__, (self.key,))
