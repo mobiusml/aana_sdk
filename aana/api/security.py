4664d6442ceee318da22c0149339c7800bf6b4c4
Aleksandr Movchan <aleksandr@mobiuslabs.com>
2025-01-29 14:46:49 +0000

    Refactor admin permissions check to respect API service settings

--- aana/api/security.py
+++ aana/api/security.py
@@ -2,6 +2,7 @@ from typing import Annotated
 
 from fastapi import Depends, Request
 
+from aana.configs.settings import settings as aana_settings
 from aana.exceptions.api_service import AdminOnlyAccess
 
 
@@ -14,10 +15,11 @@ def check_admin_permissions(request: Request):
     Raises:
         AdminOnlyAccess: If the user is not an admin
     """
-    api_key_info = request.state.api_key_info
-    is_admin = api_key_info.get("is_admin", False)
-    if not is_admin:
-        raise AdminOnlyAccess()
+    if aana_settings.api_service.enabled:
+        api_key_info = request.state.api_key_info
+        is_admin = api_key_info.get("is_admin", False)
+        if not is_admin:
+            raise AdminOnlyAccess()
 
 
 class AdminCheck:
