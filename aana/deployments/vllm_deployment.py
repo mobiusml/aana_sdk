ff3642299b2ea44cf419bb1c6506c4f9e271d5fc
Appu Shaji <appoose@gmail.com>
2025-02-22 20:36:18 +0000

    content_format at parse_chat_message

--- aana/deployments/vllm_deployment.py
+++ aana/deployments/vllm_deployment.py
@@ -191,7 +191,7 @@ class VLLMDeployment(BaseDeployment):
             images = None
 
         conversation, mm_data = parse_chat_messages(
-            messages, self.model_config, self.tokenizer
+            messages, self.model_config, self.tokenizer, content_format="default"
         )
 
         if isinstance(self.tokenizer, MistralTokenizer):
